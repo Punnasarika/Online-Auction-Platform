12651c02a5c061ef5d7e1a80dfcfc927f3d41dcd
Punna Sarika <punnasarika@example.com>
2025-04-08 14:44:31 +0530

    Added complete project

---
+++ frontend/src/components/About.js
@@ -0,0 +1,15 @@
+// src/components/About.js
+import React from 'react';
+function About(){
+  return (
+    <div>
+      <h2>About Auction App</h2>
+      <p>
+        Our Auction App provides a platform for users to buy and sell items through bidding.
+        It ensures transparency, fair bidding, and a user-friendly experience.
+      </p>
+    </div>
+  );
+}
+
+export default About;
